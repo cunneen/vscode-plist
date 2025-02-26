c825cba12c46a5d497ed65f56035f3c9440ed99f
Dave Nicolson <david.nicolson@gmail.com>
2025-02-26 19:55:40 +0100

    fix: local workspace issue

--- src/common/utilities/vscode.ts
+++ src/common/utilities/vscode.ts
@@ -3,13 +3,6 @@ import * as vscodeUri from 'vscode-uri';
 
 export const UriUtils = vscodeUri.Utils;
 
-export function isLocalWorkspace(): boolean {
-  return (
-    vscode.workspace.workspaceFolders !== undefined &&
-    vscode.workspace.workspaceFolders.every(f => f.uri.scheme === 'file')
-  );
-}
-
 export function getConfiguration<T>(
   id: string,
   defaultValue?: T
