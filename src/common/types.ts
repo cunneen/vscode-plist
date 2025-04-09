fe65b84999d242068d88318aabfe860537c6a30a
Dave Nicolson <david.nicolson@gmail.com>
2025-04-09 10:28:51 +0200

    fix: bigint support (#3) (Thanks @dnicolson)

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,4 +1,11 @@
-type SingularPlistValue = Buffer | Date | string | number | boolean | null;
+type SingularPlistValue =
+  | BigInt
+  | Buffer
+  | Date
+  | string
+  | number
+  | boolean
+  | null;
 type PlistArray = PlistValue[];
 export type PlistDictionary = {[key: string]: PlistValue};
 export type PlistValue = SingularPlistValue | PlistArray | PlistDictionary;
